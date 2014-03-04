8526b7a2100bbaacec44109daf892142d2a57289
jsimoneau <cyberdjay@hotmail.com>
2014-03-04 16:26:05 -0700

    edited codeFrame

--- src/codeFrame.cpp
+++ src/codeFrame.cpp
@@ -1,1 +1,12 @@
 //codeFrame.cpp
+#include "codeFrame.h"
+
+codeFrame::codeFrame(QWidget *parent): QWidget(parent){
+   setFixedSize(154, 12);
+}
+
+void codeBlock::paintEvent(QPaintEvent *){
+   QPainter painter(this);
+   painter.drawRect(1,1,width()-2, height()-2);
+
+}
