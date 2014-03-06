7c674152370c0ee47dd4d15b3e67fcbf15728aa4
EMacks <erin.mackey00@gmail.com>
2014-03-06 14:53:37 -0700

    snowman melts

--- src/snowman.cpp
+++ src/snowman.cpp
@@ -3,11 +3,33 @@
 #include "snowman.h"
 
 snowman::snowman(QWidget *parent) : QWidget(parent){
-   setFixedSize(204,308);
+    setFixedSize(204,308);
+    strs=new QStringList();
+    strs->append(":/images/resources/snowman_A_0.png");
+    strs->append(":/images/resources/snowman_B_0.png");
+    strs->append(":/images/resources/snowman_C_0.png");
+    strs->append(":/images/resources/snowman_D_0.png");
+    count=0;
+    image=new QImage();
+    image->load(strs->at(count));
+
+    timer=new QTimer(this);
+    timer->setInterval(3000);
+    connect(timer,SIGNAL(timeout()),this,SLOT(nextSnowman()));
+    timer->start();
+    if(count>3) {return;}
 }
 
+
 void snowman::paintEvent(QPaintEvent *){
-   QPainter painter(this);
-   QRect rect(0,0,width(),height());
-   painter.drawImage(rect,QImage(":/images/resources/snowman_A_0.png"));
+    QPainter painter(this);
+    QRect rect(0,0,width(),height());
+    painter.drawImage(rect,*image);
+}
+void snowman::nextSnowman(){
+    if (count<3){count++;}
+    image->load(strs->at(count));
+
+    repaint();
+    timer->start();
 }
--- src/snowman.h
+++ src/snowman.h
@@ -4,15 +4,27 @@
 #include <QWidget>
 #include <QString>
 #include <QPainter>
+#include <QTimer>
+#include <QImage>
+#include <QMessageBox>
 
 class snowman :public QWidget{
     Q_OBJECT
 
 public:
    snowman(QWidget *parent=0);
+//   void loadPic(const QString &name);
+public slots:
+   void nextSnowman();
    
 protected:
    void paintEvent(QPaintEvent *e);
+
+private:
+   QTimer *timer;
+   QImage *image;
+   int count;
+   QStringList *strs;
 };
 
 
