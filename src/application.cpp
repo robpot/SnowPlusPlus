8e185e8ec0ed79ddff0a7d41e5f22446a1d68a8e
EMacks <erin.mackey00@gmail.com>
2014-03-11 16:41:23 -0600

    added timer, snowman melts

--- src/application.cpp
+++
@@ -1,9 +0,0 @@
-//application.cpp
-#include "application.h"
-
-application::application(QWidget *parent) : QWidget(parent)
-{
-  game = new snowPlusPlus(this);
-  setFixedSize(1024,576);
-  game->show();
-}
