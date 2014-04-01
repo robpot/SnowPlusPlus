59db5c335e1f81630eff692681bd373c269ddfd4
Jesze Kaszas <jesze@forestbear.net>
2014-04-01 16:18:38 -0600

    Animated the gameover screen

--- src/gameOverScreen.cpp
+++ src/gameOverScreen.cpp
@@ -1,14 +1,16 @@
 #include "gameOverScreen.h"
-
-gameOverScreen::gameOverScreen(QWidget *parent, int base, int timerem, int snowrem) :QWidget(parent)
+#include <QDebug>
+gameOverScreen::gameOverScreen(QWidget *parent, int base, int timerem, int snowrem, int dif)
+  : QGraphicsScene(parent)
 {
-  setFixedSize(1024,576);
+  setSceneRect(0,0,1024,576);
   int scorespacing = 16;
   int scorefontsize = 16;
   QFont font("Times",scorefontsize);
   setFont(font);
   score.append("Total Score: ");
-  //Add score via formula
+  totalscore = base+(timerem*(dif+1))-(snowrem-1);
+  score.append(QString::number(totalscore));
   basescore.append("Base Score: ");
   basescore.append(QString::number(base));
   timeremaining.append("Time Bonus: ");
@@ -16,63 +18,122 @@ gameOverScreen::gameOverScreen(QWidget *parent, int base, int timerem, int snowr
   snowflakeamount.append("Snowflakes Remaining: ");
   snowflakeamount.append(QString::number(snowrem));
 
-  scoreintimer = new QTimer(this);
-  movebaserect = new QTimer(this);
-  movetimerect = new QTimer(this);
-  movesnowflakerect = new QTimer(this);
-  movesnowman = new QTimer(this);
+  step1done = false;
+  step2done = false;
+  step3done = false;
 
-  snowman.load(":/images/resources/frostyhorse.png");
-  backdrop.load(":/images/resources/backdrop_winter.png");
-  snowflake.load(":/images/resources/snowflake.png");
-  scorebanner.load(":/images/resources/scorebanner.png");
-  goodjobbubble.load(":/images/resources/goodjobbubble.png");
+  tic = new QTimer(this);
+  connect(tic,SIGNAL(timeout()),this,SLOT(animate()));
 
-  snowmanrect = new QRect(width()/2,576-512,512,512);
-  snowflakerect = new QRect(0,0,32,32);
-  scorebannerrect = new QRect(31,31,450,64);
-  backdroprect = new QRect(0,0,width(),height());
-  speechrect = new QRect((width()/2)+128,0,128,128);
-  speechtextrect = new QRect((width()/2)+128,6,116,116);
-  basescorerect = new QRect(31,120+scorefontsize+scorespacing,256,24);
-  timeremainingrect = new QRect(31,144+scorefontsize+scorespacing,256,24);
-  snowflakeamountrect = new QRect(31,168+scorefontsize+scorespacing,256,24);
-  scorerect = new QRect(64,192+scorefontsize+scorespacing,256,32);
+  tic->start(4);
+
+  snowman = new QGraphicsPixmapItem();
+  snowman->setPixmap(QPixmap(":/images/resources/frostyhorse.png"));
+  backdrop = new QGraphicsPixmapItem();
+  backdrop->setPixmap(QPixmap(":/images/resources/backdrop_winter.png"));
+  scorebanner = new QGraphicsPixmapItem();
+  scorebanner->setPixmap(QPixmap(":/images/resources/scorebanner.png"));
+  goodjobbubble = new QGraphicsPixmapItem();
+  goodjobbubble->setPixmap(QPixmap(":/images/resources/goodjobbubble.png"));
+
+
+  backdrop->setPos(0,0);
+  addItem(backdrop);
+
+  QGraphicsPixmapItem* whiteback = new QGraphicsPixmapItem();
+  whiteback->setPixmap(QPixmap(":/images/resources/dropspace.png"));
+  whiteback->setPos(0,0);
+  whiteback->setOpacity(0.8);
+  addItem(whiteback);
+
+  snowmanfinalx = width()/2;
+  snowmanfinaly = height()-(width()/2);
+  snowmanstartx = width();
+  snowmanstarty = height()-(width()/2);
+  snowman->setPos(snowmanstartx,snowmanstarty);
+  addItem(snowman);
+
+  scorefinalx = 31;
+  scorefinaly = 31;
+  scorestartx = 31;
+  scorestarty = -scorebanner->pixmap().height();
+  qDebug()<<scorestarty;
+
+  scorebanner->setPos(scorestartx,scorestarty);
+  addItem(scorebanner);
+
+
+  basescorerect = new QGraphicsTextItem(basescore);
+  basescorerect->setPos(31,120+scorefontsize+scorespacing);
+  timeremainingrect = new QGraphicsTextItem(timeremaining);
+  timeremainingrect->setPos(31,144+scorefontsize+scorespacing);
+  snowflakeamountrect = new QGraphicsTextItem(snowflakeamount);
+  snowflakeamountrect->setPos(31,168+scorefontsize+scorespacing);
+  scorerect = new QGraphicsTextItem(score);
+  scorerect->setPos(64,192+scorefontsize+scorespacing);
+  scorerect = new QGraphicsTextItem(score);
+  scorerect->setPos(64,192+scorefontsize+scorespacing);
 
   goodjobmessage.append("That solution sure was cool, boy howdy!");
 
-  exitscreen = new QPushButton("Continue",this);
-  exitscreen->setFixedSize(128,64);
-  exitscreen->move(512-exitscreen->width(),height()-exitscreen->height()-32);
-  connect(exitscreen,SIGNAL(clicked()),this,SLOT(closeAndSaveScore()));
 }
 
 void gameOverScreen::paintEvent(QPaintEvent *)
 {
-  QPainter painter(this);
-  painter.drawImage(*backdroprect,backdrop);
-  QRect whiteback(0,0,width()/2,height()/2);
-  painter.setOpacity(0.8);
-  painter.drawImage(whiteback,QImage(":/images/resources/dropspace.png"));
-  painter.setOpacity(1.0);
-
-  QFont font("Times",12);
-  painter.setFont(font);
-  painter.drawImage(*speechrect,goodjobbubble);
-  painter.drawText(*speechtextrect,goodjobmessage,QTextOption(Qt::AlignCenter));
-
-  QFont font2("Times",16);
-  painter.setFont(font2);
-  painter.drawImage(*snowmanrect,snowman);
-  painter.drawImage(*scorebannerrect,scorebanner);
-  painter.drawImage(*snowflakerect,snowflake);
-  painter.drawText(*scorerect,score);
-  painter.drawText(*basescorerect,basescore);
-  painter.drawText(*timeremainingrect,timeremaining);
-  painter.drawText(*snowflakeamountrect,snowflakeamount);
 }
 
 void gameOverScreen::closeAndSaveScore()
 {
-  close();
+  emit sendexit();
+}
+
+void gameOverScreen::animate()
+{
+  if(!step1done)
+  {
+    if(snowman->x() > snowmanfinalx)
+      snowman->moveBy(-1,0);
+    else
+    {
+        goodjobbubble->setPos((width()/2)+128,0);
+        addItem(goodjobbubble);
+        QGraphicsTextItem* text = new QGraphicsTextItem(goodjobmessage);
+        text->setTextWidth(116);
+        text->setFont(QFont("Times",12));
+        text->setPos((width()/2)+128+16,24);
+        addItem(text);
+        step1done = true;
+    }
+  }
+  if(!step2done)
+  {
+    if(scorebanner->y() < scorefinaly)
+      scorebanner->moveBy(0,1);
+    else
+    {
+        basescorerect->setFont(QFont("Times",16));
+        timeremainingrect->setFont(QFont("Times",16));
+        snowflakeamountrect->setFont(QFont("Times",16));
+        scorerect->setFont(QFont("Times",24));
+        if(basescorerect->scene() == 0)
+          addItem(basescorerect);
+        else if(timeremainingrect->scene() == 0)
+          addItem(timeremainingrect);
+        else if(snowflakeamountrect->scene() == 0)
+          addItem(snowflakeamountrect);
+        else
+          step2done = true;
+    }
+  }
+  if(step1done && step2done && !step3done)
+  {
+    if(scorerect->scene() == 0)
+      addItem(scorerect);
+    exitscreen = new QPushButton("Continue");
+    exitscreen->setFixedSize(128,64);
+    exitscreen->move(512-exitscreen->width(),height()-exitscreen->height()-32);
+    addWidget(exitscreen);
+    connect(exitscreen,SIGNAL(clicked()),this,SLOT(closeAndSaveScore()));
+    step3done = true;
+  }
 }
--- src/gameOverScreen.h
+++ src/gameOverScreen.h
@@ -5,23 +5,36 @@
 #include <QPushButton>
 #include <QTimer>
 #include <QPainter>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 
-class gameOverScreen : public QWidget
+class gameOverScreen : public QGraphicsScene
 {
   Q_OBJECT
 public:
-  explicit gameOverScreen(QWidget *parent = 0, int base = 0, int timerem = 0, int snowrem = 0);
+  explicit gameOverScreen(QWidget *parent = 0, int base = 0, int timerem = 0, int snowrem = 0, int dif = 0);
 private:
-  QTimer *scoreintimer, *movebaserect, *movetimerect, *movesnowflakerect,*movesnowman;
-  QImage snowman,snowflake,scorebanner,backdrop,goodjobbubble;
-  QRect *snowmanrect,*snowflakerect,*scorebannerrect,*backdroprect,*speechrect, *speechtextrect;
-  QRect *scorerect, *basescorerect, *timeremainingrect, *snowflakeamountrect;
+  QTimer *tic;
+  QGraphicsPixmapItem *snowman,*snowflake,*scorebanner,*backdrop,*goodjobbubble;
+  QGraphicsTextItem *scorerect, *basescorerect, *timeremainingrect, *snowflakeamountrect;
   QString score,goodjobmessage,basescore,timeremaining,snowflakeamount;
   QPushButton* exitscreen;
+  int snowmanstartx,snowmanfinalx;
+  int snowmanstarty,snowmanfinaly;
+
+  int scorestartx,scorestarty;
+  int scorefinalx,scorefinaly;
+  int totalscore;
+
+  bool step1done, step2done, step3done;
+
 protected:
   void paintEvent(QPaintEvent *);
+signals:
+  void sendexit();
 public slots:
   void closeAndSaveScore();
+  void animate();
 
 };
 
